a1736e21b7d7ae2422b950b2f82a7265dc9b9d5a
haku7a <v.a.neyaskin@gmail.com>
2026-04-27 14:13:29 +0300

    feat: add frontend for system info

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 import platform
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "System info app is running"}
+@app.get("/", response_class=HTMLResponse)
+def frontend():
+    return """
+    <!DOCTYPE html>
+    <html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Информация о компьютере</title>
+    </head>
+    <body>
+
+        <button onclick="loadInfo()">Инфо</button>
+
+        <pre id="result"></pre>
+
+        <script>
+            async function loadInfo() {
+                const response = await fetch('/api/info/');
+                const data = await response.json();
+
+                document.getElementById('result').textContent =
+                    JSON.stringify(data, null, 4);
+            }
+        </script>
+    </body>
+    </html>
+    """
+
+
 
 
 @app.get("/api/info/")
