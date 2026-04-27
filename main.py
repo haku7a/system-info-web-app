a161053f1d3d03b03303b6afe5530c5cf97bcb02
haku7a <v.a.neyaskin@gmail.com>
2026-04-27 14:01:17 +0300

    feat: add system info endipoint

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+import platform
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "System info app is running"}
+
+
+@app.get("/api/info/")
+def get_info():
+    return {
+        "os": {
+            "system": platform.system(),
+            "version": platform.version(),
+            "platform": platform.platform()
+        },
+
+            }
